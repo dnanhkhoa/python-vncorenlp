b715047b8f1b46ffe70b5813f9c7b46944cc966c
Anh Khoa <dnanhkhoa@live.com>
2018-05-27 21:51:24 +0700

    Update

--- vncorenlp/__init__.py
+++ vncorenlp/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from .vncorenlp import VNCORENLP_SERVER
 from .vncorenlp import VnCoreNLP
-from .vncorenlp import VnCoreNLPServer
--- vncorenlp/commandline.py
+++ vncorenlp/commandline.py
@@ -4,18 +4,18 @@ import os
 import subprocess
 import sys
 
-from vncorenlp import VnCoreNLPServer
+from vncorenlp import VNCORENLP_SERVER
 
 
 def main():
     try:
-        if not os.path.isfile(VnCoreNLPServer):
+        if not os.path.isfile(VNCORENLP_SERVER):
             raise FileNotFoundError('File "VnCoreNLPServer.jar" was not found, please re-install this package.')
 
         if subprocess.call(['java', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True):
             raise FileNotFoundError('Java was not found, please install first.')
 
-        args = ['java', '-Xmx2g', '-jar', VnCoreNLPServer]
+        args = ['java', '-Xmx2g', '-jar', VNCORENLP_SERVER]
 
         if len(sys.argv) > 1:
             pos = 1
