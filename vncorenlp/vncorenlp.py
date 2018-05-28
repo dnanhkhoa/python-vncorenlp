2946b97faa16f7a9485a38b951f4c910e9d5a13f
Anh Khoa <dnanhkhoa@live.com>
2018-05-29 01:58:27 +0700

    Update vncorenlp

--- vncorenlp/vncorenlp.py
+++ vncorenlp/vncorenlp.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
-import re
 import subprocess
+import time
 
 import requests
 from requests.exceptions import RequestException
@@ -15,23 +15,35 @@ VNCORENLP_SERVER = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath
 
 
 class VnCoreNLP(object):
-    def __init__(self, address='0.0.0.0', port=None, timeout=30, annotators='wseg,pos,ner,parse', quiet=True):
+    def __init__(self, address='0.0.0.0', port=None, annotators='wseg,pos,ner,parse', timeout=30, quiet=True):
         self.logger = logging.getLogger(__name__)
 
-        self.annotators = re.split('\s*,\s*', annotators.strip())
+        self.annotators = annotators
         self.url = None
         self.timeout = timeout
 
+        # Check if server file exists
         if not os.path.isfile(VNCORENLP_SERVER):
             raise FileNotFoundError('File "VnCoreNLPServer.jar" was not found, please re-install this package.')
 
+        # Check if Java exists
         if subprocess.call(['java', '-version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True):
-            raise FileNotFoundError('Java was not found, please install JRE 1.8 first.')
+            raise FileNotFoundError('Java was not found, please install JRE or JDK 1.8 first.')
 
         args = ['java', '-Xmx2g', '-jar', VNCORENLP_SERVER]
+
+        # Start server
         self.process = None  # subprocess.Popen(args)
 
+        # Waiting until the server is available
+        while not self.is_alive():
+            break
+            self.logger.info('Waiting until the server is available.')
+            time.sleep(5)
+        self.logger.info('The server is available.')
+
     def close(self):
+        # Stop server and clean up
         if self.process:
             self.logger.info(__class__.__name__ + ': cleaning up...')
             self.logger.info(__class__.__name__ + ': killing VnCoreNLPServer process (%s)...' % self.process.pid)
@@ -43,6 +55,7 @@ class VnCoreNLP(object):
             self.logger.info(__class__.__name__ + ': done.')
 
     def is_alive(self):
+        # Check if server is alive
         try:
             response = requests.get(url=self.url, timeout=self.timeout)
             response.raise_for_status()
@@ -57,11 +70,24 @@ class VnCoreNLP(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
-    def annotate(self, text, annotators):
-        response = requests.post()
+    def annotate(self, text, annotators=None):
+        if annotators:
+            pass
+        else:
+            annotators = self.annotators
+
+        data = {
+            'text': text.encode('UTF-8'),
+            'props': annotators
+        }
+        response = requests.post(url=self.url + '/handle', data=data, timeout=self.timeout)
+        response.raise_for_status()
+        response = response.json()
+        # status:bool
+        return response.get('result', None)
 
     def tokenize(self, text):
-        pass
+        annotated_text = self.annotate(text=text)
 
     def pos_tag(self, text):
         pass
@@ -70,4 +96,7 @@ class VnCoreNLP(object):
         pass
 
     def dep_parse(self, text):
+        pass
+
+    def detect_language(self, text):
         pass
