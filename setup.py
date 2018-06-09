81190206be4166a2b6876ad77df1538e89bfbf19
Anh Khoa <dnanhkhoa@live.com>
2018-06-09 21:57:32 +0700

    Update

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup
+
+
+def readme(file_name):
+    if os.path.isfile(file_name):
+        with open(file_name, 'rb') as f:
+            return f.read().decode('UTF-8')
+    return None
+
+
+setup(name='vncorenlp',
+      version='1.0.0',
+      description='A Python wrapper for VnCoreNLP using a bidirectional communication channel.',
+      long_description=readme(file_name='README.md'),
+      keywords='python-vncorenlp vncorenlp nlp vietnamese-nlp parser word-segmentation tokenizer pos-tagger '
+               'named-entity-recognition ner dependency-parser',
+      url='https://github.com/dnanhkhoa/python-vncorenlp',
+      author='Khoa Duong',
+      author_email='dnanhkhoa@live.com',
+      license='MIT',
+      packages=['vncorenlp'],
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=['requests'],
+      entry_points={
+          'console_scripts': ['vncorenlp=vncorenlp.commandline:main']
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Topic :: Text Processing :: Linguistic'
+      ])
